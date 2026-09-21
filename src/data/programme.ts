b65a86d516a2c2b9da69b57cba13802c218039c7
Pauline MQP <paulinemqp@MacBook-Pro-de-Pauline-3.local>
2026-09-21 11:32:41 +0200

    Actualise le texte des deux pôles de compétences

--- src/data/programme.ts
+++ src/data/programme.ts
@@ -3,7 +3,7 @@ export const programme = [
   {
     picto: 'main',
     titre: ['2 pôles de', 'compétences'],
-    intro: 'Acquisition et application des compétences psychosociales pour soutenir la santé mentale et sensibilisation au bien-être numérique.',
+    intro: 'Acquisition et application de compétences émotionnelles et relationnelles clés pour soutenir les difficultés de santé mentale. Sensibilisation et accompagnement à un rapport plus sain au numérique.',
   },
   {
     picto: 'asterisque',
